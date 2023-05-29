c0fb5c4afc157cc5722233468fd85e7ba22d115c
Marco <marco.bejarano03@gmail.com>
2023-05-28 21:40:55 -0700

    fix the issues that were being caused with the openai module

--- chatroom_Ai/members/urls.py
+++ chatroom_Ai/members/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('members/', views.members, name='members'),
+    path('chat/', views.chat_view),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
 ]
--- chatroom_Ai/members/views.py
+++ chatroom_Ai/members/views.py
@@ -1,25 +1,50 @@
+import os
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
-from .models import Member, ChatMessage
+from .models import ChatMessage
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from dotenv import load_dotenv
 import openai
 
-openai.api_key = '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'
+load_dotenv()
 
-def members(request):
-  if request.method == 'POST':
-    message = request.POST.get('message')
+openai.api_key = os.getenv('api_key')
 
-    ChatMessage.objects.create(message=message)
+@csrf_exempt
+def chat_view(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+
+        # Store the user message
+        ChatMessage.objects.create(message=message)
+
+        # Generate a response using ChatGPT
+        response = openai.Completion.create(
+            engine='text-davinci-003',
+            prompt=message,
+            max_tokens=50,
+            temperature=0.7,
+            n=1,
+            stop=None,
+        )
+
+        response_text = response.choices[0].text.strip()
+
+        # Store the ChatGPT response
+        ChatMessage.objects.create(message=response_text)
+
+        return JsonResponse({'response': response_text})
+
+    return render(request, 'index.html')
+
 
 def signup(request):
   return render(request, 'signup.html')
 
 def signin(request):
   return render(request, 'signin.html')
-
 
 def main(request):
   template = loader.get_template('main.html')
